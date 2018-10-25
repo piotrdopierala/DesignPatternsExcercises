759c5dbef1e5980c6189ce26fb4a71e2ad59d917
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-10-25 05:34:15 +0200

    added mediatedUpdate property in UIObjects to prevent notofication loops

--- src/pl/dopierala/Behavioral/DP_Mediator/Label.java
+++ src/pl/dopierala/Behavioral/DP_Mediator/Label.java
@@ -4,22 +4,26 @@ public class Label implements UIControl {
     private UIMediator mediator;
     private String value;
     private String name;
+    private boolean mediatedUpdate; //changing value because of mediator notify, don't notify other object its a LOOP! Break the loop.
+
 
     public Label(String name, UIMediator mediator) {
         this.mediator = mediator;
-        this.name=name;
+        this.name = name;
         mediator.register(this);
     }
 
     @Override
     public void controlChanged(UIControl uiObject) {
-        System.out.println(name+": notified that "+uiObject.getName()+" changed.");
-        System.out.println(name+": New value is " + uiObject.getValue() + ".");
+        System.out.println(name + ": notified that " + uiObject.getName() + " changed.");
+        System.out.println(name + ": New value is " + uiObject.getValue() + ".");
+        mediatedUpdate = false;
     }
 
     public void setValue(String value) {
         this.value = value;
-        mediator.valueChanged(this);
+        if (!mediatedUpdate)
+            mediator.valueChanged(this);
     }
 
     @Override
